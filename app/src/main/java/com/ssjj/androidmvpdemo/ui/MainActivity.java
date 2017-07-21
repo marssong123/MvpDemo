bd6d34fa83612fc518532df12d5a84a86a684877
songyu1 <songyu1@yy.com>
2017-07-21 14:18:08 +0800

    test commit

--- app/src/main/java/com/ssjj/androidmvpdemo/ui/MainActivity.java
+++ app/src/main/java/com/ssjj/androidmvpdemo/ui/MainActivity.java
@@ -2,6 +2,8 @@ package com.ssjj.androidmvpdemo.ui;
 /**
  * Created by songmars on 16/8/26.
  */
+
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -37,6 +39,38 @@ public class MainActivity extends MvpActivity<MainPresenter> implements MainView
         text.setText(new MyJni().getString());
     }
 
+    public Point[] kClosest(Point[] points, Point origin, int k) {
+        // Write your code here
+        Point[] points1 = new Point[100];
+        for (int j = 1; j < points.length; j++) {
+            for (int i = 0; i < points.length - 1; j++) {
+
+                if ((points[i].x-origin.x) *(points[i].x-origin.x) + (points[i].y-origin.y)*(points[i].y-origin.y) < (points[i + 1].x-origin.x) *(points[i + 1].x-origin.x) + (points[i + 1].y-origin.y) *(points[i + 1].x-origin.y)) {
+                    Point t = points[i + 1];
+                    points[i + 1] = points[i];
+                    points[i] = t;
+                } else if ((points[i].x-origin.x) *(points[i].x-origin.x) + (points[i].y-origin.y)*(points[i].y-origin.y) == (points[i + 1].x-origin.x) *(points[i + 1].x-origin.x) + (points[i + 1].y-origin.y) *(points[i + 1].x-origin.y)) {
+                    if (points[i].x < points[i + 1].x) {
+                        Point t = points[i + 1];
+                        points[i + 1] = points[i];
+                        points[i] = t;
+                    } else if (points[i].x == points[i + 1].x) {
+                        if (points[i].y < points[i + 1].y) {
+                            Point t = points[i + 1];
+                            points[i + 1] = points[i];
+                            points[i] = t;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            points1[i] = points[i];
+        }
+        return points1;
+    }
+
     @Override
     protected MainPresenter createPresenter() {
         return new MainPresenter(this);
@@ -47,10 +81,10 @@ public class MainActivity extends MvpActivity<MainPresenter> implements MainView
     public void getDataSuccess(BaseBean bean) {
         //接口成功回调
 
-        HotWordsBean hotWordsBean= (HotWordsBean) bean;
-        String hotlist="";
-        for(String hotword:hotWordsBean.getData()){
-            hotlist=hotlist.concat(hotword+"\n");
+        HotWordsBean hotWordsBean = (HotWordsBean) bean;
+        String hotlist = "";
+        for (String hotword : hotWordsBean.getData()) {
+            hotlist = hotlist.concat(hotword + "\n");
         }
 //        text.setText(hotlist);
         Toast.makeText(mActivity, "request success", Toast.LENGTH_SHORT).show();
