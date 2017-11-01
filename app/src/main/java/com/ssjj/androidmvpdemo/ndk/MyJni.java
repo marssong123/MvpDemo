c20973c9cd5e9df335214b79d86c650775bb702f
songyu <songyu1@yy.com>
2017-11-01 19:50:05 +0800

    mylog

--- app/src/main/java/com/ssjj/androidmvpdemo/ndk/MyJni.java
+++ app/src/main/java/com/ssjj/androidmvpdemo/ndk/MyJni.java
@@ -1,13 +1,16 @@
 package com.ssjj.androidmvpdemo.ndk;
 
+import android.util.Log;
+
 /**
  * Created by songmars on 2017/7/5.
  */
 
 public class MyJni {
-
+private static final String TAG = "MyJni" ;
     static {
         System.loadLibrary("MyLibrary");
+        Log.d(TAG, "static initializer: ");
     }
 
     public  native String getString();
